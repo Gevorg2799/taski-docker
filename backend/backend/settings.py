8bec2bff3bc513c4122f4e6066a02a124766d16a
GEV <gevorg2799@mail.ru>
2024-07-24 16:42:19 +0300

    update Flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,6 @@
-from pathlib import Path
+
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['123.123.123.123',  'localhost',
+ALLOWED_HOSTS = ['123.123.123.123', 'localhost',
                  '158.160.66.128', 'gevproject.hopto.org']
 
 
